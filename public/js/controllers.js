441e7de64965071bb87c0238908e9d4373f04b4b
Jason <jhostetter@gmail.com>
2013-09-28 12:42:59 -0400

    first test

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,25 +3,14 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http) {
+	controller('AppCtrl', function ($scope, $http) {
 
-    $http({
-      method: 'GET',
-      url: '/api/name'
-    }).
-    success(function (data, status, headers, config) {
-      $scope.name = data.name;
-    }).
-    error(function (data, status, headers, config) {
-      $scope.name = 'Error!'
-    });
+		$scope.findHue = function(){
+			$http.get('www.meethue.com/api/nupnp')
+				.success(function(result){
+					console.log(result)
+					$scope.output = result
+				})
+		}
 
-  }).
-  controller('MyCtrl1', function ($scope) {
-    // write Ctrl here
-
-  }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
-
-  });
+	})
