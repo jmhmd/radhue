2606322030e6826a4532ad948e566e45d377bd4e
jjmmd <JJM@Jamess-MacBook-Pro.local>
2013-10-23 01:55:52 -0400

    Cleaned code
    Removed legacy code
    Added comments

--- public/js/lib/hue.js
+++ public/js/lib/hue.js
@@ -1,50 +1,45 @@
-var connectIP;
-var dataStruct = {
-					"on" : null,
-					"bri" : null,
-					"hue" : null,
-					"sat" : null,
-					"xy" : [null, null],
-					"ct" : null,
-					"alert" : "",
-					"effect" : ""
-				};
+var HueIP;
 
 $( function() {
+	$('.demo').each( function() {
+        $(this).minicolors({
+            control: $(this).attr('data-control') || 'hue',
+                                
+            change: function(hex, opacity) {
+            var log;
+            try {
+                log = hex ? hex : 'transparent';
+                if( opacity ) log += ', ' + opacity;
+                    colorChange(hex);
+            	} catch(e) {}
+        	},
+        	theme: 'default'
+        });
+    });
+
+    findHue();
+});
+
+//Find the Hue Bridge and set the IP address to global value HueIP
+function findHue() {
 	$.get("http://www.meethue.com/api/nupnp", function(result, textStatus){
     	if(textStatus = "success") {
-    		connectIP = result[0].internalipaddress;
-    		console.log("Hue Bridge Found: " + connectIP);
+    		HueIP = result[0].internalipaddress;
+    		console.log("Hue Bridge Found: " + HueIP);
     	}
     	else {
     		console.log("No Hue Bridge is identified");
     	}
     });
-                        
-            $('.demo').each( function() {
-                $(this).minicolors({
-                    control: $(this).attr('data-control') || 'hue',
-                                        
-                    change: function(hex, opacity) {
-                    var log;
-                    try {
-                        log = hex ? hex : 'transparent';
-                        if( opacity ) log += ', ' + opacity;
-                            colorChange(hex);
-                    } catch(e) {}
-                },
-                theme: 'default'
-            });
-                
-        });
-                        
-});
+}
 
-
+//Get the Lighting Information from the Hue Bridge and create HTML representations of each light
 function infoLight() {
-	$.get("http://" + connectIP + "/api/newdeveloper", function(result, status) {
+	$.get("http://" + HueIP + "/api/newdeveloper", function(result, status) {
 		if(status == "success") {
+			//Iterate once for each light
 			for(var i in result.lights) {
+				//Add a On/Off Button, Brightness slider, and Colorwheel
 				$('#lightMenu').append('<li>' + result.lights[i].name + '<br>' + 
 					'<button id=' + i + ' onclick="buttonClick(this.id)">  ' + result.lights[i].state.on + '</button><br>' +
 					'<input type="range" id=' + i + 'slider min=0 max=255 step=1 onmouseup="sliderMove(this.id, this.value)"></input>' + 
@@ -52,22 +47,23 @@ function infoLight() {
 			}
 		}
 	});
-
-	$.get("http://" + connectIP + "/api/newdeveloper", function(result, status) {
-		if(status == "success") {
-			dataStruct = result.lights[1].state;
-		}
-	});
 };
 
+//Change the light color according to the value returned from the colorpicker
+//Unfortunately the value is sent as a 6 digit hex number which does not correspond to the 
+//color values coded in the Hue
 function colorChange(value) {
+	//Eliminate the leading '#'
 	value = value.replace('#', '');
 		console.log(": " + parseInt(value, 16) / 256);
+
 	var brightness = new Object();
+	//Tried to transform the hex number into something hue friendly, but doesn't work well at all
 	brightness.hue = Math.round(parseInt(value, 16) / 256);
 
+	//Update the light
 	$.ajax({
-	    url: 'http://' + connectIP + '/api/newdeveloper/lights/1/state',
+	    url: 'http://' + HueIP + '/api/newdeveloper/lights/1/state',
 	    data : JSON.stringify(brightness),
 	    type: 'PUT',
 		success: function(result) {
@@ -76,12 +72,14 @@ function colorChange(value) {
 	});
 }
 
+//This function is called when the brightness is changed by moving the slider
+//Slider values are between 0 (dark) and 255 (bright)
 function sliderMove(lightID, value) {
 	var brightness = new Object();
 	brightness.bri = parseInt(value);
 
 	$.ajax({
-	    url: 'http://' + connectIP + '/api/newdeveloper/lights/' + lightID[0] + '/state',
+	    url: 'http://' + HueIP + '/api/newdeveloper/lights/' + lightID[0] + '/state',
 	    data : JSON.stringify(brightness),
 	    type: 'PUT',
 		success: function(result) {
@@ -90,31 +88,11 @@ function sliderMove(lightID, value) {
 	});
 }
 
-function toggleLight() {
-	var resultBox = $('#annotationResult');
-	var switchName = $('#Lightswitch');
-
-	if(dataStruct.on == true) {
-		dataStruct.on = false;
-		switchName.html("Light ON")
-	}
-	else {
-		dataStruct.on = true;
-		switchName.html("Light OFF")
-	}
-
-	$.ajax({
-	    url: 'http://' + connectIP + '/api/newdeveloper/lights/1/state',
-	    data : JSON.stringify(dataStruct),
-	    type: 'PUT',
-	    success: function(result) {
-	        //console.log(JSON.stringify(result));
-	    }
-	});
-};
-
+//Toggle the light on or off.  This makes a get call to the light to see what the on state is and
+//then reverses it. The ajax call is performed in the .done so that it runs in sequence rather than
+//asynchronously
 function buttonClick(lightID) {
-	$.get("http://" + connectIP + "/api/newdeveloper", function(result, status) {
+	$.get("http://" + HueIP + "/api/newdeveloper", function(result, status) {
 		if(status == "success") {
 			if(result.lights[lightID].state.on == true) {
 				result.lights[lightID].state.on = false;
@@ -125,7 +103,7 @@ function buttonClick(lightID) {
 		}
 	}).done( function(result) {
 		$.ajax({
-		    url: 'http://' + connectIP + '/api/newdeveloper/lights/' + lightID + '/state',
+		    url: 'http://' + HueIP + '/api/newdeveloper/lights/' + lightID + '/state',
 		    data : JSON.stringify(result.lights[lightID].state),
 		    type: 'PUT',
 			success: function(result) {
@@ -134,17 +112,3 @@ function buttonClick(lightID) {
 		});
 	});
 }
-
-function getHueIP(){
-	var resultBox = $('#annotationResult');
-
-	$.get("http://" + connectIP + "/api/newdeveloper", function(result, status) {
-		if(status == "success") {
-			dataStruct = result.lights[1].state;
-
-			resultBox.html(JSON.stringify(dataStruct));
-		}
-	});
-
-	//resultBox.html(JSON.stringify(dataStruct));
-};
